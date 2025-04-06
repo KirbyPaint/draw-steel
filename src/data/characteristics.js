6bff87167fca18002fce2e296809636681e5444b
KirbyPaint <porter.ashley13@gmail.com>
2025-04-05 20:33:51 -0700

    add barest bones stuff and testing

---
+++ src/data/characteristics.js
@@ -0,0 +1,27 @@
+export const characteristics = [
+	{
+		name: 'Might',
+		description: 'Might represents strength and brawn. A creature\'s ability to break down doors, swing an axe, stand up during an earthquake, or hurl an ally across a chasm is determined by Might.',
+		abbreviation: 'M',
+	},
+	{
+		name: 'Agility',
+		description: 'Agility represents coordination and nimbleness. A creature\'s ability to backflip out of danger, shoot a crossbow, dodge an explosion, or pluck  eys from a guard\'s belt is determined by Agility.',
+		abbreviation: 'A',
+	},
+	{
+		name: 'Reason',
+		description: 'Reason represents a logical mind and education. A creature\'s ability to solve a puzzle that unlocks a door, recall lore about necromancy,  decipher a coded message, or blast a foe with psionic power is determined by Reason.',
+		abbreviation: 'R',
+	},
+	{
+		name: 'Intuition',
+		description: 'Intuition represents instincts and experience. A creature\'s ability to recognize a faint sound as the approach of a distant rider, quickly read the tell of a bluffing gambler, calm a rearing horse, or track a monster across the tundra is determined by Intuition.',
+		abbreviation: 'I',
+	},
+	{
+		name: 'Presence',
+		description: 'Presence represents force of personality. A creature\'s ability to lie to a judge, convince a crowd to join a revolution, impress a queen at   royal banquet, or cast a magic spell by singing a song is determined by Presence',
+		abbreviation: 'P',
+	},
+];
