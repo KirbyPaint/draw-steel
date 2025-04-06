c1589c9f80d5db413d081ada7aea1cc1f07e0887
KirbyPaint <porter.ashley13@gmail.com>
2025-04-06 10:19:23 -0700

    fix button styling and placement for mobile

--- src/scripts.js
+++ src/scripts.js
@@ -57,3 +57,13 @@ function changeButtonColor(buttonId) {
 		console.error(`Button with ID ${buttonId} not found.`);
 	}
 }
+
+function refresh() {
+	// Will restore all buttons to their original state
+	const buttons = document.querySelectorAll('.buttonToggle');
+	buttons.forEach(button => {
+		if (button.classList.contains('off')) {
+			button.classList.remove('off');
+		}
+	});
+}
